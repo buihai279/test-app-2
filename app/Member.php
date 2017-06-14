3cf371136abb2ff5850aef41dd194c8c5e24375f
Bui Van Hai <haibv1377@dhcnhn.edu.vn>
2017-06-14 17:57:40 +0700

    14 - June 2017,CRUD angularjs

--- app/Member.php
+++ app/Member.php
@@ -7,6 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Member extends Model
 {
     protected $table='members';
+
+    protected $fillable = ['name','age','address'];
+
+    protected $guarded = ['id','created_at','updated_at'];
+
     public $timestamps = true;
     
 }
